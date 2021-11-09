cf29cf6290c65f769ea2c2188f3aec64d8089d9b
michitatara <torterra10@gmail.com>
2021-11-08 21:23:47 -0500

    env3

--- myApp.js
+++ myApp.js
@@ -4,14 +4,17 @@ var app = express();
 module.exports = app;
 console.log('Hello World');
 app.get("/json", function(req, res) {
-    
-    
-   const response = "Hello json";
+if(process.env.VAR_NAME === "allCaps"){
+    response = "Hello json".toUpperCase();
+} else{
+    response = "Hello json";
+}
+   /*const response = "Hello json";
    if (process.env.VAR_NAME === "uppercase"){
        res.json({ message: response.toUpperCase() });
    } else{
        res.json({message: response});
    }
-});
+});*/
 
 app.use("/public",express.static(__dirname + "/public"))
