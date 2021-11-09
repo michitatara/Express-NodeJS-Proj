dcecbded5e9c4d83f0126978b9810b985c5756f1
michitatara <torterra10@gmail.com>
2021-11-08 23:27:20 -0500

    time

--- myApp.js
+++ myApp.js
@@ -14,9 +14,20 @@ if(process.env.MESAGE_STYLE === "uppercase"){
         {"message": "Hello json"}
     )
 }
-});*/
+});
   
 app.use(function(req,res,next){
     console.log(req.method + " "+ req.path+ "-" + req.ip);
     next();
+});*/
+
+function getTheCurrentTimeString(){
+    return new Date().toString();
+}
+app.get("/now", function(req,res,next) {
+    req.time = getTheCurrentTimeString();
+    next();
+}, function(req,res){
+    res.json({time: req.time});
 })
+
