70d612efeb17ce76d231cb9f3a75cd74e92f309d
michitatara <torterra10@gmail.com>
2021-11-08 22:48:31 -0500

    env5

--- myApp.js
+++ myApp.js
@@ -4,32 +4,18 @@ var app = express();
 module.exports = app;
 console.log('Hello World');
 
-let messageObject = {"message": "Hello json"};
-app.get('/json', function(req, res) {
-  if (process.env.MESSAGE_STYLE === 'uppercase') {
-     var u_=JSON.parse(JSON.stringify(messageObject ));
-     u_.message=u_.message.toUpperCase();
-     return res.json(u_);
-  } else {
-      return res.json(messageObject);
-  }
-});
-
-/*app.get("/json", function(req, res) {
-if(process.env.VAR_NAME === "allCaps"){
-    response = "Hello json".toUpperCase();
+app.get("/json", function(req, res) {
+if(process.env.MESAGE_STYLE === "uppercase"){
+    res.json(
+        {"message":"HELLO JSON"}       
+    )
 } else{
-    response = "Hello json";
+    res.json(
+        {"message": "Hello json"}
+    )
 }
 });
-   const response = "Hello json";
-   if (process.env.VAR_NAME === "uppercase"){
-       res.json({ message: response.toUpperCase() });
-   } else{
-       res.json({message: response});
-   }
-});*/
-
+  
 app.use(function middleware(req,res,next){
     req.time = Date().toString();
     console.log(req.time);
