8d984502e9c5306196b759296e472f8e0692f891
michitatara <torterra10@gmail.com>
2021-11-09 00:50:00 -0500

    4th

--- myApp.js
+++ myApp.js
@@ -53,9 +53,8 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
 //get data from post requests
-/*app.post("/name", function(req,res) {
-    res.json({name: req.body.first + " " + req.body.last});
-});*/
-app.post("/name", (req, res) => {
-    res.json({name: req.body.first + " " + req.body.last});
+app.post("/name", function(req, res) {
+    // Handle the data in the request
+    var string = req.body.first + " " + req.body.last;
+    res.json({ name: string });
   });
